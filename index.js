aafd13bec197fdec432f362a7efcc8c814276bdf
John O'Connor <oconnojb@gmail.com>
2019-04-20 21:59:13 +0000

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,38 @@ function preventRefreshOnSubmit(){
     })
 }
 
-preventRefreshOnSubmit()
+preventRefreshOnSubmit();
+
+function retrieveEmployeeInformation() {
+  let aule = $('input');
+  let melko = aule[0].value;
+  return melko;
+}
+
+function addNewElementAsLi() {
+  let aule = $('ul.employee-list');
+  aule.append($('<li>').text(retrieveEmployeeInformation()));
+  let a = $('input');
+  let melko = a[0].value;
+  a[0].value = '';
+}
+
+function addNewLiOnClick() {
+  let btn = $('input')[1];
+  btn.addEventListener('click', addNewElementAsLi());
+}
+let i=0;
+
+function clearEmpLst() {
+  if (i!=0) {
+    let aule = $('ul.employee-list');
+    aule.empty();
+  }
+}
+
+function clearEmployeeListOnLinkClick() {
+  let lnk = $('a')[0];
+
+  lnk.addEventListener('click', clearEmpLst);
+  i++;
+}
